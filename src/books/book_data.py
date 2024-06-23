9f9b3f1fad0604070afd1a1ef08e7d8758c5f33b
Amauri Clementino <amaurisantospro@gmail.com>
2024-06-22 23:28:58 -0300

    Set book data to a file

---
+++ src/books/book_data.py
@@ -0,0 +1,50 @@
+books = {
+    1: {
+        'title': 'Richard Montes',
+        'author': 'William Clark',
+        'publisher': 'Mcneil, Hall and Duke',
+        'publisher_date': '2022-11-12',
+        'page_count': 135,
+        'language': 'Albanian'
+    },
+    2: {
+        'title': 'Steven Owen',
+        'author': 'Christopher Smith',
+        'publisher': 'Anderson, Stewart and Cooper',
+        'publisher_date': '1991-08-18',
+        'page_count': 248,
+        'language': 'Tibetan'
+    },
+    3: {
+        'title': 'Lorraine Gonzalez',
+        'author': 'Michael Thornton',
+        'publisher': 'Allen, Kennedy and Brady',
+        'publisher_date': '2020-07-31',
+        'page_count': 139,
+        'language': 'Malagasy'
+    },
+    4: {
+        'title': 'Candace Burns',
+        'author': 'Roger Hill',
+        'publisher': 'Hudson LLC',
+        'publisher_date': '1992-07-08',
+        'page_count': 102,
+        'language': 'Tigrinya'
+    },
+    5: {
+        'title': 'Paul Howard MD',
+        'author': 'Lucas Brooks',
+        'publisher': 'Cohen, Logan and Carter',
+        'publisher_date': '1970-07-20',
+        'page_count': 187,
+        'language': 'Kashmiri'
+    },
+    6: {
+        'title': 'Matthew Carey',
+        'author': 'Michael Joseph',
+        'publisher': 'Reid, Gilbert and Padilla',
+        'publisher_date': '2014-07-12',
+        'page_count': 139,
+        'language': 'Bihari languages'
+    }
+}
